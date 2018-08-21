4d6eb08665d1e706ed24c20679258f85c3bd78c1
Cheol Noh <dan.noh@gmail.com>
2018-08-21 21:06:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-function takeANumber()
+function takeANumber(currentLine, newName){
+  return `Welcome, ${newName}. You are number ${currentLine.length + 1} in line.`
+}
+
+function nowServing(currentLine){
+  
+}
