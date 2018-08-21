88316e4b91c35ce275f914ec67576ff02acba201
Cheol Noh <dan.noh@gmail.com>
2018-08-21 21:30:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function currentLine(currentLineArray){
   
   else {
     for (var i = 0; i < currentLineArray.length; i++){
-    array.push(`${i+1}. ${currentLineArray[i]}`);
+    array.push(`${i+1}. ${currentLineArray[i]} `);
       }
     str = array.join();  
   }
